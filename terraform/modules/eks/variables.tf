8047e83a71b7bc6cdb559234cbecc31cd4261909
luiscornelius <luistobechukwu@gmail.com>
2026-07-18 12:03:52 +0100

    Add EKS module with cluster, IAM roles, and managed node group

---
+++ terraform/modules/eks/variables.tf
@@ -0,0 +1,28 @@
+variable "cluster_version" {
+  description = "Kubernetes version for the EKS control plane"
+  type        = string
+  default     = "1.34"
+}
+
+variable "public_subnet_ids" {
+  type = list(string)
+}
+
+variable "private_subnet_ids" {
+  type = list(string)
+}
+
+variable "node_desired_size" {
+  type    = number
+  default = 2
+}
+
+variable "node_max_size" {
+  type    = number
+  default = 4
+}
+
+variable "node_min_size" {
+  type    = number
+  default = 2
+}
